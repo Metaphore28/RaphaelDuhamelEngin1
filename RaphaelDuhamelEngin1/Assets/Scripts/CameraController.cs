73f69e17a88e614516da850d8fe072de53c61ae6
DESKTOP-N1TP8DF\rafdu <raf.duhamel@gmail.com>
2023-08-29 16:22:53 -0400

    Added functionnality where camera puts irself in fron of objects obstructing the view.

---
+++ RaphaelDuhamelEngin1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_objectToLookAt;
+    [SerializeField]
+    private float m_rotationSpeed = 1.0f;
+    [SerializeField]
+    private Vector2 m_clampingXRotationValues = Vector2.zero;
+
+    private void Awake()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateHorizontalMovements();
+        UpdateVerticalMovements();
+        UpdateCameraScroll();
+    }
+
+    void FixedUpdate()
+    {
+        FUpdateTestCameraInFrontOfObstructions();
+    }
+
+    private void UpdateHorizontalMovements()
+    {
+        float currentAngleX = Input.GetAxis("Mouse X") * m_rotationSpeed;
+        transform.RotateAround(m_objectToLookAt.position, m_objectToLookAt.up, currentAngleX);
+    }
+
+    private void UpdateVerticalMovements()
+    {
+        float currentAngleY = Input.GetAxis("Mouse Y") * m_rotationSpeed;
+        float eulersAngleX = transform.rotation.eulerAngles.x;
+
+        float comparisonAngle = eulersAngleX + currentAngleY;
+
+        comparisonAngle = ClampAngle(comparisonAngle);
+
+        if ((currentAngleY < 0 && comparisonAngle < m_clampingXRotationValues.x)
+            || (currentAngleY > 0 && comparisonAngle > m_clampingXRotationValues.y))
+        {
+            return;
+        }
+        transform.RotateAround(m_objectToLookAt.position, transform.right, currentAngleY);
+    }
+
+    private void UpdateCameraScroll()
+    {
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            //TODO: Faire une vérification selon la distance la plus proche ou la plus éloignée
+            //Que je souhaite entre ma caméra et mon objet
+
+            //TODO: Lerp plutôt que d'effectuer immédiatement la translation
+            transform.Translate(Vector3.forward * Input.mouseScrollDelta.y, Space.Self);
+        }
+    }
+
+    private void FUpdateTestCameraInFrontOfObstructions()
+    {
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        var vecteurDiff = transform.position - m_objectToLookAt.position;
+        var distance = vecteurDiff.magnitude;
+        if (Physics.Raycast(m_objectToLookAt.position, vecteurDiff, out hit, distance, layerMask))
+        {
+            // Un objet entre focus et camera
+            Debug.DrawRay(m_objectToLookAt.position, vecteurDiff.normalized * hit.distance, Color.yellow);
+            transform.SetPositionAndRotation(hit.point, transform.rotation);
+        }
+        else
+        {
+            // Pas d'objet
+            Debug.DrawRay(m_objectToLookAt.position, vecteurDiff, Color.white);
+        }
+    }
+
+    private float ClampAngle(float angle)
+    {
+        if (angle > 180)
+        {
+            angle -= 360;
+        }
+        return angle;
+    }
+}
